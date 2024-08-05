f2856d37f1b607e21d7c1ce43c88d81a61538d5f
VoThiKimGiau <vothikimgiau16122003@gmail.com>
2024-08-05 22:51:33 +0700

    Kết nối cơ sở dữ liệu + test hiển thị dữ liệu khách hàng

--- QuanLyBanSach_Web/QuanLyBanSach_Web/Controllers/HomeController.cs
+++ QuanLyBanSach_Web/QuanLyBanSach_Web/Controllers/HomeController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using QuanLyBanSach_Web.Models;
 
 namespace QuanLyBanSach_Web.Controllers
 {
     public class HomeController : Controller
     {
+        QuanLyBanSachEntities db = new QuanLyBanSachEntities();
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            List<KhachHang> dsKH = db.KhachHang.ToList();
+            return View(dsKH);
         }
     }
 }
